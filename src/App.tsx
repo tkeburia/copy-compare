033802ab7418c21dfc6d9618f64b0003a08981d3
Tornike Keburia <tornike.keburia@red-badger.com>
2023-05-18 12:06:37 +0100

    Initialize project using Create React App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useEffect, useState } from "react";
 import { Button, LinearProgress } from "@mui/material";
 import "./App.css";
@@ -101,6 +102,29 @@ function App() {
           rightTitle="NEW"
         />
       )}
+=======
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+>>>>>>> b4c74b6 (Initialize project using Create React App)
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -13,4 +13,10 @@ root.render(
   </React.StrictMode>
 );
 
+<<<<<<< HEAD
+=======
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+>>>>>>> b4c74b6 (Initialize project using Create React App)
 reportWebVitals();
